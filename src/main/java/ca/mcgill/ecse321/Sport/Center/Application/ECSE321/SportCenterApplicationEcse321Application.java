59f682ef7f0b4f477fcde8ca106c19adb1ed64e2
Behrad Rezaie <r.behrad121@gmail.com>
2024-02-11 14:14:55 -0500

    spring boot stuff

---
+++ src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/SportCenterApplicationEcse321Application.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.Sport.Center.Application.ECSE321;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportCenterApplicationEcse321Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportCenterApplicationEcse321Application.class, args);
+	}
+
+}
---
+++ src/test/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/SportCenterApplicationEcse321ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.Sport.Center.Application.ECSE321;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCenterApplicationEcse321ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
