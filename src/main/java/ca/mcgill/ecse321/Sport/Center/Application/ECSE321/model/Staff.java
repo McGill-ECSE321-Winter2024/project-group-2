09a5229dea103534eaf069df5a33f22f25723973
Goldrelia <aurelia.bouliane@gmail.com>
2024-02-15 15:56:14 -0500

    JPA annotations for Staff

--- src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/model/Staff.java
+++ src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/model/Staff.java
@@ -1,18 +1,25 @@
 package ca.mcgill.ecse321.Sport.Center.Application.ECSE321.model;
 
+import java.io.Serializable;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+
 /*PLEASE DO NOT EDIT THIS CODE*/
 
 /*This code was generated using the UMPLE 1.33.0.6934.a386b0a58 modeling language!*/
 
 // line 27 "model.ump"
 // line 116 "model.ump"
-public abstract class Staff extends Role {
+@Entity
+public abstract class Staff extends Role implements Serializable {
 
   // ------------------------
   // MEMBER VARIABLES
   // ------------------------
 
   // Staff Attributes
+  @EmbeddedId
   private int employeeId;
 
   // ------------------------
