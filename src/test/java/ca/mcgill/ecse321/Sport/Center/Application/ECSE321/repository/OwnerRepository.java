0afa2b9c7ed7387151c3a8a5af6c19202030bc75
SReinhardt283 <sebastian.reinhardt@videotron.ca>
2024-02-17 17:53:08 -0500

    Added the 3 missing repository test classes

---
+++ src/test/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/repository/OwnerRepository.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse321.Sport.Center.Application.ECSE321.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OwnerRepository {
+    
+}
