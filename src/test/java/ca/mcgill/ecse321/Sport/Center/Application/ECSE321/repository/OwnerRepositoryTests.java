f05ba2744c5b7893b5bdcf757a9724eeadb87893
SReinhardt283 <sebastian.reinhardt@videotron.ca>
2024-02-17 17:56:36 -0500

    Trolled the naming :(

--- src/test/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/repository/OwnerRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/repository/OwnerRepositoryTests.java
@@ -3,6 +3,6 @@ package ca.mcgill.ecse321.Sport.Center.Application.ECSE321.repository;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ClassTypeRepository {
+public class OwnerRepositoryTests {
     
 }
