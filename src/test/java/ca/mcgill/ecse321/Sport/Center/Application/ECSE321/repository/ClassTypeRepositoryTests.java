dfa9f6c214da24d71a2828eef08fe3a5747d4fb9
Goldrelia <aurelia.bouliane@gmail.com>
2024-02-18 15:10:26 -0500

    Deleted some unused imports (clean up)

--- src/test/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/repository/ClassTypeRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/repository/ClassTypeRepositoryTests.java
@@ -9,10 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.ClassTypeRepository;
-
-import java.sql.Date;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
