b3da666ff6d85b22828ce5b9fd54bf61d932cccd
behradrez <r.behrad121@gmail.com>
2024-02-27 20:33:44 -0500

    added reject class type method template

--- src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/SportCenterApplicationEcse321Application.java
+++ src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/SportCenterApplicationEcse321Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SportCenterApplicationEcse321Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SportCenterApplicationEcse321Application.class, args);
 	}
 
