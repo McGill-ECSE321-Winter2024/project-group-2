cad65e2452305570d7e98873cc58db9a23faaa53
YuriSorice <yuri.sorice@mail.mcgill.ca>
2024-03-01 14:12:32 -0500

    new service file for creating and log in account

---
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/service/AccountService.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse321.Sport.Center.Application.ECSE321.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.InstructorRepository;
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.OwnerRepository;
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.PersonRepository;
+
+@Service
+public class AccountService {
+    @Transactional
+    public void createAccount(){
+        return;
+    }
+    
+    @Transactional
+    public void login(){
+        return;
+    }
+}
