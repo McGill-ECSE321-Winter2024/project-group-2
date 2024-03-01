ed7a250eeb1153773aa26dc6c7f29182511af16c
YuriSorice <yuri.sorice@mail.mcgill.ca>
2024-03-01 14:12:56 -0500

    added autowired

--- SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/service/AccountService.java
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/service/AccountService.java
@@ -5,11 +5,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.InstructorRepository;
-import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.OwnerRepository;
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.CustomerRepository;
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.PersonRepository;
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.OwnerRepository;
 
 @Service
 public class AccountService {
+    
+    @Autowired
+    PersonRepository personRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    InstructorRepository instructorRepository;
+    @Autowired
+    OwnerRepository ownerRepository;
+
     @Transactional
     public void createAccount(){
         return;
