9e9a974aad7dced2da81c7bbe588dc39b819d765
YuriSorice <yuri.sorice@mail.mcgill.ca>
2024-03-11 14:21:36 -0400

    working on account and scheduling services

--- SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/dto/PersonDTO.java
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/dto/PersonDTO.java
@@ -1,18 +1,31 @@
 package ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dto;
 
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.model.Person;
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.model.Role;
+
 public class PersonDTO {
     private int personId;
     private String password;
     private String email;
     private String name;
+    private String role;
 
     public PersonDTO() {
     }
-    public PersonDTO(int personId, String password, String email, String name) {
+    public PersonDTO(int personId, String password, String email, String name, Role role ) {
         this.personId = personId;
         this.password = password;
         this.email = email;
         this.name = name;
+        this.role = role.toString();
+    }
+
+    public PersonDTO(Person model) {
+        this.personId = model.getPersonId();
+        this.password = model.getPassword();
+        this.email = model.getEmail();
+        this.name = model.getName();
+        this.role = model.getRole().toString();
     }
 
     public int getPersonId() {
@@ -38,6 +51,14 @@ public class PersonDTO {
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
     
 }
