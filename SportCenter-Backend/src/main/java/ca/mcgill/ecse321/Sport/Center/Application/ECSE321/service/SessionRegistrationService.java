64b37858676a91301abaadafc5d70ab6b2d98a1d
Goldrelia <aurelia.bouliane@gmail.com>
2024-03-11 15:58:24 -0400

    Alice and Aurelia finished SessionRegistrationService.java

--- SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/service/SessionRegistrationService.java
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/service/SessionRegistrationService.java
@@ -8,7 +8,9 @@ import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.CustomerRepository
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.PersonRepository;
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.SessionRegistrationRepository;
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.SessionRepository;
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.model.Customer;
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.model.Session;
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.model.SessionRegistration;
 
 @Service
 public class SessionRegistrationService {
@@ -21,24 +23,53 @@ public class SessionRegistrationService {
     @Autowired
     private SessionRegistrationRepository sessionRegistrationRepository;
     
-
+    /**
+     * 
+     * @return
+     * @author Aurelia Bouliane
+     */
     @Transactional
     public Iterable<Session> viewSessions(){
         return sessionRepository.findAll();
     }
 
+    /**
+     * 
+     * @param aId
+     * @param aSession
+     * @param aCustomer
+     * @return
+     * @author Alice, Aurelia
+     */
     @Transactional
-    public void registerForSession(){
-        return;
-    }
-    
-    @Transactional
-    public void viewSpecificSession(){
-        return;
+    public SessionRegistration registerForSession(int aId, Session aSession, Customer aCustomer){
+        SessionRegistration sessionRegistration = new SessionRegistration(aId, aSession, aCustomer);
+        return sessionRegistrationRepository.save(sessionRegistration);
     }
 
+    /**
+     * 
+     * @param pid
+     * @return
+     * @throws Exception
+     * @author Alice Godbout
+     */
     @Transactional
-    public void cancelRegistration(){
-        return;
+    public SessionRegistration viewSpecificSession(int pid) throws Exception {
+        SessionRegistration s = sessionRegistrationRepository.getSessionRegistrationById(pid);
+        if (s == null) {
+            throw new Exception("There is no person with this ID.");
+    } 
+        return s;
+    }
+    
+    /**
+     * 
+     * @param id
+     * @author Aurelia, Alice
+     */
+    @Transactional
+    public void cancelRegistration(int id){
+        sessionRegistrationRepository.deleteById(id);
     }
 }
