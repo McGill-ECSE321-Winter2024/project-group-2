4c04cf69c111c398aabe4e67d8cbd770afb7317e
peiya <pei.geng@mail.mcgill.ca>
2024-03-14 22:17:10 -0400

    started schedule controller

--- SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/controller/ScheduleController.java
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/controller/ScheduleController.java
@@ -1,5 +1,15 @@
 package ca.mcgill.ecse321.Sport.Center.Application.ECSE321.controller;
 
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.service.SchedulingService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ScheduleController {
-    
+    @Autowired
+    private SchedulingService service;
 }
