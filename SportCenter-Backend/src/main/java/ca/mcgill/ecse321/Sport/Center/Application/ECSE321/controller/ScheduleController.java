bc49c313b8f883fd9cf820a43755fdf3ed4ca39d
peiya <pei.geng@mail.mcgill.ca>
2024-03-16 10:32:56 -0400

    fixed schedule controller

--- SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/controller/ScheduleController.java
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/controller/ScheduleController.java
@@ -6,6 +6,8 @@ import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.service.SchedulingServ
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dto.SessionDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -17,21 +19,30 @@ public class ScheduleController {
     private SchedulingService service;
 
     @PostMapping("/sessions")
-    public SessionResponseDTO createSession(@RequestBody SessionDTO request) {
+    public ResponseEntity<SessionResponseDTO> createSession(@RequestBody SessionDTO request) {
         Session newSession = service.createSession(request.getId(), request.getLength(), request.getStartTime(), request.getEndTime(), request.getDate(), request.getIsRepeating(), request.getMaxParticipants(), request.getClassType(), null);
-        return new SessionResponseDTO(newSession);
+        if (newSession == null) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(new SessionResponseDTO(newSession), HttpStatus.CREATED);
     }
+
     @GetMapping("/sessions/{id}")
-    public SessionResponseDTO findSessionById(@PathVariable int id) {
+    public ResponseEntity<?> findSessionById(@PathVariable int id) {
         Session session = service.findSessionById(id);
-        return SessionResponseDTO.create(session);
+        if (session == null) {
+            return new ResponseEntity<>("Session not found", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(SessionResponseDTO.create(session));
     }
+
     @GetMapping("/sessions")
-    public List<SessionResponseDTO> findAllEvents() {
-        List<SessionResponseDTO> dtos = new ArrayList<SessionResponseDTO>();
+    public List<SessionResponseDTO> findAllSessions() {
+        List<SessionResponseDTO> dtos = new ArrayList<>();
         for (Session s : service.findAllSessions()) {
             dtos.add(SessionResponseDTO.create(s));
         }
         return dtos;
     }
 }
+
