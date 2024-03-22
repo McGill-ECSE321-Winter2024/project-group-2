5812a259041f40c6edfc5bcd4375e12f190ce952
peiyan-geng <143831847+peiyan-geng@users.noreply.github.com>
2024-03-21 22:34:27 -0400

    added integration test skeleton for sessions

---
+++ SportCenter-Backend/src/test/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/integration/SessionIntegrationTests.java
@@ -0,0 +1,33 @@
+package ca.mcgill.ecse321.Sport.Center.Application.ECSE321.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dto.SessionDTO;
+import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dto.SessionResponseDTO;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(Lifecycle.PER_CLASS)
+public class SessionIntegrationTests{
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    
+}
