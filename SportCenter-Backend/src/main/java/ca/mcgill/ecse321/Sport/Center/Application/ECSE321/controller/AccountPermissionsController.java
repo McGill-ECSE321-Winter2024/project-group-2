75cfe3eaf8f3238492957ea1d2851305fc9a64e7
behradrez <r.behrad121@gmail.com>
2024-03-23 19:28:30 -0400

    for seb my love

--- SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/controller/AccountPermissionsController.java
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/controller/AccountPermissionsController.java
@@ -2,9 +2,11 @@ package ca.mcgill.ecse321.Sport.Center.Application.ECSE321.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.dao.PersonRepository;
@@ -18,29 +20,31 @@ public class AccountPermissionsController {
     @Autowired
     AccountPermissionsService service;
 
-    @PutMapping("/persons/{id}")
-    public ResponseEntity<?> grantInstructorPermissions(@PathVariable("id") String id) throws Exception{
+    @PostMapping("/instructors")
+    public ResponseEntity<?> grantInstructorPermissions(@RequestBody String id) throws Exception{
         int idAsInt = 0;
+        InstructorDTO instructor = null;
         try {
             idAsInt = Integer.parseInt(id);
+            instructor = service.grantInstructorPermissions(idAsInt);
+
         }
         catch(NumberFormatException num){
             return new ResponseEntity<>("Bad integer value", HttpStatus.BAD_REQUEST);
         }
         catch (Exception e) {
-            if (e.getMessage().contains("not found")){
+            if (e.getMessage().contains("Person does not exist")){
                 return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-            }
-
-            else if (e.getMessage().contains("person already is an instructor")){
+            }else if (e.getMessage().contains("Person is already an instructor")){
                 return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            }else{
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_GATEWAY);
             }
         }
-        InstructorDTO instructor = service.grantInstructorPermissions(idAsInt);
         return new ResponseEntity<>(instructor, HttpStatus.OK);
     }
 
-    @PutMapping("/instructors/{id}")
+    @DeleteMapping("/instructors/{id}")
     public ResponseEntity<?> revokeInstructorPermissions(@PathVariable("id") String id){
         try {
             int idAsString = Integer.parseInt(id);
