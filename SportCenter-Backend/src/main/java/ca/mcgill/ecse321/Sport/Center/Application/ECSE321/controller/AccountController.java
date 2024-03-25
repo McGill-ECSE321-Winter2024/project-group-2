31b0b7a6aa9067a488ba23be319833579c3d6f7c
Goldrelia <aurelia.bouliane@gmail.com>
2024-03-24 20:26:09 -0400

    Code cleanup!

--- SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/controller/AccountController.java
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/controller/AccountController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.Sport.Center.Application.ECSE321.service.AccountService;
--- SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/service/AccountService.java
+++ SportCenter-Backend/src/main/java/ca/mcgill/ecse321/Sport/Center/Application/ECSE321/service/AccountService.java
@@ -131,8 +131,6 @@ public class AccountService {
      */
     @Transactional
     public PersonDTO createPerson(String password, String email, String name) {
-          //TODO make all valid checks for password, duplicate email, etc. Remember to throw a SportCenterException
-
         if(isNullOrEmpty(password) || isNullOrEmpty(email) || isNullOrEmpty(name)){
             throw new IllegalArgumentException("Password, email, and name cannot be empty");
         }
