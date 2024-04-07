981fa23b04193e8e49bbefcf3a4ff095b9cef491
Behrad Rezaie <r.behrad121@gmail.com>
2024-04-07 14:55:27 -0400

    added class type rejection and approval

--- SportCenter-Frontend/src/components/OwnerPage.vue
+++ SportCenter-Frontend/src/components/OwnerPage.vue
@@ -1,47 +1,66 @@
 <template>
     <div id="sportcenter">
-        <h2>Promote/Demote Instructors</h2>
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Instructor Id</th>
-                <th>Person Id</th>
-            </tr>
-            <tr v-for="person in currentView" :key="person.name">
-                <td>{{ person.name  }}</td>
-                <td>
-                    {{ person.instructorId }}
-                </td>
-                <td>
-                    {{ person.personId }}
-                </td>
-                <td>
-                    <button v-if="person.instructorId=='N/A'" @click="promoteToInstructor(Number(person.personId))">Promote to Instructor</button>
-                    <button v-if="person.instructorId!='N/A'" @click="demoteInstructor(person.instructorId)">Demote Instructor</button>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input v-model="newPerson" type="text" placeholder="Person Name">
-                </td>
-                <td>
-                    <button @click="getPersons()">Show all people</button>
-                    <button @click="getInstructors()">Show all instructors</button>
-                </td>
-                <td>
-                    <input v-model="newInstructor" type="number" placeholder="Person ID to promote">
-                </td>
-                <td>
-                    <button v-bind:disabled="!newInstructor" @click='promoteToInstructor(newInstructor)'>Promote</button>
-                </td>
-            </tr>
-        </table>
-        <p>
-            <span v-if="successMessage" style="color:green">{{ successMessage }}</span> 
-        </p>
-        <p>
-            <span v-if="errorMessage" style="color:red">Error: {{ errorPerson }}</span>
-        </p>
+        <div id="grantAccountPermissions">
+            <h2>Promote/Demote Instructors</h2>
+            <button @click="getPersons()">View non-instructor accounts</button>
+            <button @click="getInstructors()">View instructors accounts</button>
+            <br>
+            <input v-model="newInstructor" type="number" placeholder="Customer's personID to promote">
+            <button v-bind:disabled="!newInstructor" @click='promoteToInstructor(newInstructor)'>Promote</button>
+            <br>
+            <h4 class="error" v-if="currentView.length==0">No people found!</h4>
+            <table v-if="currentView.length!=0" class="center">
+                <tr>
+                    <th class="rowName">Name</th>
+                    <th>Instructor id number</th>
+                    <th>Person account number</th>
+                </tr>
+                <tr v-for="person in currentView" :key="person.name">
+                    <td>{{ person.name  }}</td>
+                    <td>
+                        #{{ person.instructorId }}
+                    </td>
+                    <td>
+                        #{{ person.personId }}
+                  </td>
+                    <td>
+                        <button v-if="person.instructorId=='N/A'" @click="promoteToInstructor(Number(person.personId))">Promote to Instructor</button>
+                        <button v-if="person.instructorId!='N/A'" @click="demoteInstructor(person.instructorId)">Demote Instructor</button>
+                    </td>
+                </tr>
+            </table>
+            <p>
+                <span v-if="instructorSuccessMessage" style="color:green">{{ instructorSuccessMessage }}</span> 
+            </p>
+            <p>
+                <span v-if="instructorErrorMessage" style="color:red">Error: {{ instructorErrorMessage }}</span>
+            </p>
+        </div>
+        <div>
+            <h2>Approve/Disapprove Class Types</h2>
+            <h4 class="error" v-if="suggestedClassTypes.length==0"> No suggested class types to review!</h4>
+            <table v-else class="center">
+
+                <tr>
+                    <th>Suggested Class Type</th>
+                </tr>
+                <tr v-for="classType in suggestedClassTypes">
+                    <td>
+                        {{ classType.name }}
+                    </td>
+                    <td>
+                        <button @click="approveClassType(classType.name)">Approve</button>
+                        <button @click="rejectClassType(classType.name)">Disapprove</button>
+                    </td>
+                </tr>
+            </table>
+            <p>
+                <span v-if="typeSuccessMessage" style="color:green">{{ typeSuccessMessage }}</span>
+            </p>
+            <p>
+                <span v-if="typeErrorMessage" style="color:red">Error: {{ typeErrorMessage }}</span>
+            </p>
+        </div>
     </div>
 </template>
 
@@ -49,9 +68,23 @@
 
 </script>
 <style>
+ 
   span {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     color: #2c3e50;
     background: #f2ece8;
+  }
+  .rowName{
+    margin-right: 300px;
+  }
+  th{
+    padding-right:15px;
+  }
+  .error{
+    color:red;
+  }
+  .center{
+    margin-left: auto;
+    margin-right: auto;
   }
 </style>
--- SportCenter-Frontend/src/components/registration.js
+++ SportCenter-Frontend/src/components/registration.js
@@ -21,13 +21,10 @@ function InstructorDTO(instructorId, personName, personId){
     this.personId = personId;
 }
 
-function EventDTO(name,date,start,end){
+function classType(name){
     this.name = name;
-    this.eventDate = date;
-    this.startTime = start;
-    this.endTime = end;
 }
-let tempArr =[];
+
 let customers = [];
 export default {
     name: 'eventregistration',
@@ -36,11 +33,16 @@ export default {
             customers: [],
             instructors: [],
             currentView: [],
-            
             newPerson: '',
             newInstructor:'',
-            errorMessage: '',
-            successMessage:'',
+            instructorErrorMessage: '',
+            instructorSuccessMessage:'',
+
+            approvedClassTypes: [],
+            suggestedClassTypes: [],
+            typeSuccessMessage: '',
+            typeErrorMessage: '',
+
             response: []
         }
     },
@@ -76,9 +78,22 @@ export default {
                 }
             }
             this.currentView = this.customers
-            this.successMessage = 'Persons loaded successfully'
+            this.instructorSuccessMessage = 'Persons loaded successfully'
         }).catch(e => {
-            this.errorMessage = e
+            this.instructorErrorMessage = e
+        })
+
+        this.classTypes = []
+        AXIOS.get('/classTypes/false')
+        .then(response => {
+            for(let i=0; i<response.data.length; i++) {
+                this.suggestedClassTypes.push(new classType(response.data[i].name))
+            }
+        })
+        .catch(e => {
+            const errorMsg = e.response.data.message
+            this.instructorErrorMessage = errorMsg
+            console.log(errorMsg)
         })
 
     },
@@ -98,9 +113,9 @@ export default {
                     }
                 }
                 this.currentView = this.customers
-                this.successMessage = 'Persons loaded successfully'
+                this.instructorSuccessMessage = 'Persons loaded successfully'
             }).catch(e => {
-                this.errorMessage = e
+                this.instructorErrorMessage = e
             })
         },
         getInstructors: function(){
@@ -114,12 +129,12 @@ export default {
                     })
                 }
                 this.currentView = this.instructors
-                this.successMessage = 'Instructors loaded successfully'
+                this.instructorSuccessMessage = 'Instructors loaded successfully'
                 console.log(response.data)
             })
             .catch(e => {
                 const errorMsg = e.response.data.message
-                this.errorMessage = errorMsg
+                this.instructorErrorMessage = errorMsg
                 console.log(errorMsg)
             })
         },
@@ -129,7 +144,7 @@ export default {
             .then(response => {
                 console.log(response.data)
                 this.newInstructor = ''
-                this.successMessage = 'Instructor added successfully'
+                this.instructorSuccessMessage = 'Instructor added successfully'
                 for(let i=0; i<this.customers.length; i++){
                     if(this.customers[i].personId == personId){
                         this.instructors.push(this.customers[i])
@@ -140,7 +155,7 @@ export default {
                 console.log(e)
                 const errorMsg = e.response.data.message
                 console.log(errorMsg)
-                this.errorMessage = errorMsg
+                this.instructorErrorMessage = errorMsg
             })
         },
         demoteInstructor: function (instructorId){
@@ -154,15 +169,54 @@ export default {
                 console.log(response.data)
                 this.successMessage = 'Instructor removed successfully'
                 if(response.status!=200){
-                    this.errorMessage = response.data.message
+                    this.instructorErrorMessage = response.data.message
                     console.log("hellooooo")
                 }
             }).catch(e =>{
                 const errorMsg = e.response.data.message
-                this.errorMessage = errorMsg
+                this.instructorErrorMessage = errorMsg
                 console.log(errorMsg)
             })
-        }
+        },
+        
+        approveClassType: function (classTypeName){
+            AXIOS.put('/classTypes/'.concat(classTypeName).concat('/true'))
+            .then(response =>{
+                console.log(response.data)
+                this.instructorSuccessMessage = 'Class type approved successfully'
+                for(let i=0; i<this.suggestedClassTypes.length; i++){
+                    if(this.suggestedClassTypes[i].name == classTypeName){
+                        this.approvedClassTypes.push(this.suggestedClassTypes[i])
+                        this.suggestedClassTypes.splice(i,1)
+                    }
+                }
+                this.typeSuccessMessage = 'Class type approved successfully'
+            })
+            .catch(e => {
+                const errorMsg = e.response.data.message
+                this.typeErrorMessage = errorMsg
+                console.log(errorMsg)
+            })
+        },
+
+        rejectClassType: function (classTypeName){
+            AXIOS.put('/classTypes/'.concat(classTypeName).concat('/false'))
+            .then(response =>{
+                console.log(response.data)
+                this.typeSuccessMessage = 'Class type rejected successfully'
+                for(let i=0; i<this.suggestedClassTypes.length; i++){
+                    if(this.suggestedClassTypes[i].name == classTypeName){
+                        this.suggestedClassTypes.splice(i,1)
+                    }
+                }
+
+            })
+            .catch(e => {
+                const errorMsg = e.response.data.message
+                this.typeErrorMessage = errorMsg
+                console.log(errorMsg)
+            })
+        },
 
     }
 }
