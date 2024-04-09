97ddaa48aab5b81093cb9a9a0571964de1760199
aliceg01 <alice.godbout@mail.mcgill.ca>
2024-04-09 18:53:35 -0400

    myacount js and vue

---
+++ SportCenter-Frontend/src/components/MyAccount.vue
@@ -0,0 +1,96 @@
+<template>
+    <div id="sportcenter">
+        <div id="yourInfo">
+            <h2>Your Information</h2>
+            <br>
+            <table class="center">
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                <tr>
+                    <td>{{ user.person.id }}</td>
+                    <td>{{ user.person.name }}</td>
+                    <td>{{ user.person.email }}</td>
+                </tr>
+            </table>
+        </div>
+        <div id="grantAccountPermissions">
+            <h2>Your Registrations</h2>
+            <br>
+            <h4 class="error" v-if="currentRegistrations.length==0">No registrations found!</h4>
+            <table v-if="currentRegistrations.length!=0" class="center">
+                <tr>
+                    <th class="rowName">Registration Id</th>
+                    <th>Session Id</th>
+                    <th>Length</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Date</th>
+                    <th>Class Type</th>
+                    <th>Instructor</th>
+                    <th></th>
+                </tr>
+                <tr v-for="registration in currentRegistrations" :key="registration.registrationId">
+                    <td>{{ registration.registrationId }}</td>
+                    <td>
+                        {{ registration.sessionId }}
+                    </td>
+                    <td>
+                        {{ registration.length }}
+                  </td>
+                  <td>
+                        {{ registration.startTime }}
+                  </td>
+                  <td>
+                        {{ registration.endTime }}
+                  </td>
+                  <td>
+                        {{ registration.date }}
+                  </td>
+                  <td>
+                        {{ registration.classType }}
+                  </td>
+                  <td>
+                        {{ registration.instructor }}
+                  </td>
+                    <td>
+                        <button @click="CancelRegistration(String(registration.registrationId))">Cancel Registration</button>
+                    </td>
+                </tr>
+            </table>
+            <p>
+                <span v-if="instructorSuccessMessage" style="color:green">{{ instructorSuccessMessage }}</span> 
+            </p>
+            <p>
+                <span v-if="instructorErrorMessage" style="color:red">Error: {{ instructorErrorMessage }}</span>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script src="./myaccount.js">
+
+</script>
+<style>
+ 
+  span {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+  .rowName{
+    margin-right: 300px;
+  }
+  th{
+    padding-right:15px;
+  }
+  .error{
+    color:red;
+  }
+  .center{
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
---
+++ SportCenter-Frontend/src/components/myaccount.js
@@ -0,0 +1,98 @@
+import axios, { Axios } from 'axios'
+import config from "../../config"
+
+
+const frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+const backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+const AXIOS = axios.create({
+    baseURL: backendUrl,
+    headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+function PersonDTO(name) {
+    this.name = name;
+    this.events = [],
+        this.personId;
+}
+
+function InstructorDTO(instructorId, personName, personId) {
+    this.name = personName;
+    this.instructorId = instructorId;
+    this.personId = personId;
+}
+
+function SessionRegistrationDTO(registrationId, sessionId, length, startTime, endTime, date, classType, instructor) {
+    this.registrationId = registrationId;
+    this.sessionId = sessionId;
+    this.length = length;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.date = date;
+    this.classType = classType;
+    this.instructor = instructor;
+}
+
+function classType(name) {
+    this.name = name;
+}
+
+let customers = [];
+export default {
+    name: 'eventregistration',
+    data() {
+        return {
+            customers: [],
+            instructors: [],
+            currentView: [],
+            currentRegistrations: [],
+            newPerson: '',
+            newInstructor: '',
+            instructorErrorMessage: '',
+            instructorSuccessMessage: '',
+
+            approvedClassTypes: [],
+            suggestedClassTypes: [],
+            typeSuccessMessage: '',
+            typeErrorMessage: '',
+
+            response: []
+        }
+    },
+    created: function () {
+        this.currentRegistrations = []
+        AXIOS.get('/sessionRegistrations/customers/3')
+            .then(response => {
+                for (let i = 0; i < response.data.length; i++) {
+                    this.currentRegistrations.push(new SessionRegistrationDTO(response.data[i].id, response.data[i].session.id, response.data[i].session.length, response.data[i].session.startTime, response.data[i].session.endTime, response.data[i].session.date, response.data[i].session.classType.classType, response.data[i].session.instructor.id))
+                }
+                this.instructorSuccessMessage = 'Persons loaded successfully'
+            }).catch(e => {
+                this.instructorErrorMessage = e
+            })
+            .catch(e => {
+                const errorMsg = e.response.data.message
+                this.instructorErrorMessage = errorMsg
+                console.log(errorMsg)
+            })
+
+    },
+    methods: {
+        CancelRegistration: function (registrationId) {
+            console.log(typeof registrationId)
+            AXIOS.delete('/sessionRegistrations/'.concat(registrationId))
+                .then(response => {
+                    console.log(response.data)
+                    this.successMessage = 'Instructor removed successfully'
+                    if (response.status != 200) {
+                        this.instructorErrorMessage = response.data.message
+                        console.log("hellooooo")
+                    }
+                }).catch(e => {
+                    const errorMsg = e.response.data.message
+                    this.instructorErrorMessage = errorMsg
+                    console.log(errorMsg)
+                })
+        }
+    }
+}
